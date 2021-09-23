0f1be30f5592996b8f36c5841ce6e7648a4dc4f8
owenramire <owenomar29@gmail.com>
2021-09-23 17:49:14 -0500

    Delete unuseful things

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
 import ToDoList from './src/views/ToDo/ToDoList';
 
 const App = () => {
-  return (
-      <ToDoList />
-  );
+  return <ToDoList />;
 };
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-  },
-});
 
 export default App;
